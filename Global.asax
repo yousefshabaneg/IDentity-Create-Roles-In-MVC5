a51b83d1bc94349e5d47a7141064ebffcf04e29d
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-05-04 17:48:58 +0200

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IDentity.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,49 @@
+﻿using IDentity.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IDentity.Startup))]
+namespace IDentity
+{
+    public partial class Startup
+    {
+        private ApplicationDbContext dbContext = new ApplicationDbContext();
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRoles();
+        }
+
+        public void CreateRoles()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(dbContext));
+
+            IdentityRole role;
+
+            if (!roleManager.RoleExists("Admins"))
+            {
+                role = new IdentityRole();
+                role.Name = "Admins";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Authors"))
+            {
+                role = new IdentityRole();
+                role.Name = "Authors";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Readers"))
+            {
+                role = new IdentityRole();
+                role.Name = "Readers";
+                roleManager.Create(role);
+            }
+
+        }
+    }
+}
